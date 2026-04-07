88a5477c261e4f9894fc2e7a633d3d494365a1ef
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-07 21:40:42 +0530

    new

--- mojito/src/App.jsx
+++ mojito/src/App.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText); //this line make sure that both plugins are ready to use globally
 
 const App = () => {
   return (
